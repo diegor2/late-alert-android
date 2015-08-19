43d3518daa9031e732ee836b96afaf1ca92c1318
Diego Ramos Ruggeri <drruggeri@gmail.com>
2015-08-18 22:12:41 -0300

    Location service

---
+++ app/src/main/java/net/startapi/latealert/LocationService.java
@@ -0,0 +1,18 @@
+package net.startapi.latealert;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class LocationService extends Service {
+
+    private Binder mBinder = new Binder() {
+
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+}
